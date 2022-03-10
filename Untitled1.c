037520068e97677c2709295f9a777fd68c4c984f
Tony Johnson <94525498+tonyjohnson12@users.noreply.github.com>
2022-03-10 12:18:56 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    {
+int a[10],b[10],c[10],i,j;
+for(i=0;i<10;i++)
+{
+   printf("enter a element :");
+scanf("%d",&a[i]);
+}
+for(j=0;j<10;j++)
+{
+  printf("enter a element :");
+scanf("%d",&b[i]);
+}
+c[10]=a[10]+b[10];
+printf("result");
+scanf("%d",&c[10]);
+}
+return0
+}
